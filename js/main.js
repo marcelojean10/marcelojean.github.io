44ff2d692ea7a8a3faf43a46bb327e73aa47a171
Marcelo Jean <marcelojeam1@gmail.com>
2021-07-08 15:04:27 -0300

    Update view my page github for template clean.

---
+++ js/main.js
@@ -0,0 +1,15 @@
+const myUserGithub = `marcelojean10`;
+
+fetch(`https://api.github.com/users/${myUserGithub}`, {
+    method: 'GET',
+    headers: new Headers()
+    })
+    .then(async function (response) {
+            const { avatar_url, name, company } = await response.json();
+
+            document.getElementById("photo-perfil").src = avatar_url;
+            document.getElementById("name-perfil").innerText = name;
+            document.getElementById("company-perfil").innerText = company;
+    })
+    .catch(error => console.error(error));
+
